cb69e472486446a44e6e7c01763bd81cb43197bf
Alex Vlasov <av@bankexfoundation.org>
2018-06-19 12:54:00 +0300

    set 1000 parallel workers per second

--- loadTest/createAndSpend_test.go
+++ loadTest/createAndSpend_test.go
@@ -25,10 +25,10 @@ var testAccountBinary = common.FromHex(testAccount)
 var testPrivateKey = common.FromHex("0x7e2abf9c3bcd5c08c6d2156f0d55764602aed7b584c4e95fa01578e605d4cd32")
 var amountAsString = "1000000000000000000"
 var serverAddress = "http://127.0.0.1:80"
-var concurrencyLimit = 10000
+var concurrencyLimit = 1000
 var timeout = time.Duration(60 * time.Second)
 var timesToRun = 10
-var connLimit = 5000
+var connLimit = 1000
 
 var httpClient *http.Client
 
