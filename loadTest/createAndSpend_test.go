401cff10847e56d5052df1ba4aa21e71291b2865
Alex Vlasov <av@bankexfoundation.org>
2018-06-19 16:43:54 +0300

    use 10 different accounts

--- loadTest/createAndSpend_test.go
+++ loadTest/createAndSpend_test.go
@@ -22,11 +22,34 @@ var txToCreate = 1000000
 
 // var txToCreate = 10000
 var blockNumber = int(rand.Uint32())
-var testAccount = "0xf62803ffaddda373d44b10bf6bb404909be0e66b"
-var testAccountBinary = common.FromHex(testAccount)
-var testPrivateKey = common.FromHex("0x7e2abf9c3bcd5c08c6d2156f0d55764602aed7b584c4e95fa01578e605d4cd32")
-var amountAsString = "1000000000000000000"
 
+// var testAccount = "0xf62803ffaddda373d44b10bf6bb404909be0e66b"
+// var testPrivateKey = common.FromHex("0x7e2abf9c3bcd5c08c6d2156f0d55764602aed7b584c4e95fa01578e605d4cd32")
+
+var testAccountBinary = common.FromHex("0xf62803ffaddda373d44b10bf6bb404909be0e66b")
+var amountAsString = "1000000000000000000"
+var testAccounts = []string{"0x627306090abab3a6e1400e9345bc60c78a8bef57",
+	"0xf17f52151ebef6c7334fad080c5704d77216b732",
+	"0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef",
+	"0x821aea9a577a9b44299b9c15c88cf3087f3b5544",
+	"0x0d1d4e623d10f9fba5db95830f7d3839406c6af2",
+	"0x2932b7a2355d6fecc4b5c0b6bd44cc31df247a2e",
+	"0x2191ef87e392377ec08e7c08eb105ef5448eced5",
+	"0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
+	"0x6330a553fc93768f612722bb8c2ec78ac90b3bbc",
+	"0x5aeda56215b167893e80b4fe645ba6d5bab767de"}
+
+var testPrivateKeys = [][]byte{
+	common.FromHex("c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"),
+	common.FromHex("ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"),
+	common.FromHex("0dbbe8e4ae425a6d2687f1a7e3ba17bc98c673636790f1b8ad91193c05875ef1"),
+	common.FromHex("c88b703fb08cbea894b6aeff5a544fb92e78a18e19814cd85da83b71f772aa6c"),
+	common.FromHex("388c684f0ba1ef5017716adb5d21a053ea8e90277d0868337519f97bede61418"),
+	common.FromHex("659cbb0e2411a44db63778987b1e22153c086a95eb6b18bdf89de078917abc63"),
+	common.FromHex("82d052c865f5763aad42add438569276c00d3d88a2d062d36b2bae914d58b8c8"),
+	common.FromHex("aa3680d5d48a8283413f7a108367c7299ca73f553735860a87b08f39395618b7"),
+	common.FromHex("0f62d96d6675f32685bbdb8ac13cda7c23436f63efbb9d07700d8669ff12b7c4"),
+	common.FromHex("8d5366123cb560bb606379f90a0bfd4769eecc0557f1b362dcae9012b548b1e5")}
 var serverAddress = "http://127.0.0.1:80"
 
 // var serverAddress = "http://127.0.0.1:3001"
@@ -42,8 +65,9 @@ type responseStruct struct {
 }
 
 type txCreatingResult struct {
-	txNumber int
-	success  bool
+	txNumber   int
+	success    bool
+	privateKey []byte
 }
 
 func TestServer(t *testing.T) {
@@ -54,41 +78,44 @@ func TestServer(t *testing.T) {
 
 func create(txNumber int, results chan txCreatingResult, wg *sync.WaitGroup) {
 	defer wg.Done()
+	randomAccountIndex := rand.Intn(len(testAccounts))
+	testAccount := testAccounts[randomAccountIndex]
+	testPrivateKey := testPrivateKeys[randomAccountIndex]
 	data := map[string]interface{}{"for": testAccount, "blockNumber": blockNumber, "transactionNumber": txNumber, "outputNumber": 0, "value": amountAsString}
 	body, err := json.Marshal(data)
 	req, err := http.NewRequest("POST", serverAddress+"/createUTXO", bytes.NewBuffer(body))
 	req.Header.Set("Content-Type", "application/json")
 	if err != nil {
-		results <- txCreatingResult{0, false}
+		results <- txCreatingResult{0, false, []byte{}}
 		return
 	}
 	client := httpClient
 	resp, err := client.Do(req)
 	if err != nil {
 		// fmt.Print(err)
-		results <- txCreatingResult{0, false}
+		results <- txCreatingResult{0, false, []byte{}}
 		return
 	}
 	defer resp.Body.Close()
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// fmt.Print(err)
-		results <- txCreatingResult{0, false}
+		results <- txCreatingResult{0, false, []byte{}}
 		return
 	}
 	var response responseStruct
 	err = json.Unmarshal(respBody, &response)
 	if err != nil {
 		// fmt.Print(err)
-		results <- txCreatingResult{0, false}
+		results <- txCreatingResult{0, false, []byte{}}
 		return
 	}
 	if response.Error {
 		// fmt.Print(err)
-		results <- txCreatingResult{0, false}
+		results <- txCreatingResult{0, false, []byte{}}
 		return
 	}
-	results <- txCreatingResult{txNumber, true}
+	results <- txCreatingResult{txNumber, true, testPrivateKey}
 }
 
 func spend(str string, results chan bool, wg *sync.WaitGroup) {
@@ -122,7 +149,7 @@ func spend(str string, results chan bool, wg *sync.WaitGroup) {
 	results <- !response.Error
 }
 
-func createTransferTransaction(blockNumber int, txNumberInBlock int, outputNumberInTransaction int, value string) ([]byte, error) {
+func createTransferTransaction(blockNumber int, txNumberInBlock int, outputNumberInTransaction int, value string, testPrivateKey []byte) ([]byte, error) {
 	bn := types.NewBigInt(int64(blockNumber))
 	tn := types.NewBigInt(int64(txNumberInBlock))
 	in := types.NewBigInt(int64(outputNumberInTransaction))
@@ -192,16 +219,16 @@ func run() {
 	fmt.Println("Created UTXOs")
 	chanForResults := make(chan bool, 10000000)
 	allTXes := []string{}
-	validTxNumbers := []int{}
+	validTxNumbers := []txCreatingResult{}
 	close(chanForCreate)
 	for res := range chanForCreate {
 		if res.success {
-			validTxNumbers = append(validTxNumbers, res.txNumber)
+			validTxNumbers = append(validTxNumbers, res)
 		}
 	}
 
-	for _, txNumber := range validTxNumbers {
-		a, err := createTransferTransaction(blockNumber, txNumber, 0, amountAsString)
+	for _, res := range validTxNumbers {
+		a, err := createTransferTransaction(blockNumber, res.txNumber, 0, amountAsString, res.privateKey)
 		if err != nil {
 			continue
 		}
